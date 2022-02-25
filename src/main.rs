595ec3b2301841b68ff93808191a4598bc5682f6
madoka2011 <1944285796@qq.com>
2022-02-25 18:10:43 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 struct Solution {}
 
-// 621. Task Scheduler
+// 11. Container With Most Water
 impl Solution {
-    pub fn least_interval(tasks: Vec<char>, n: i32) -> i32 {
-        let mut frqs = vec![0; 26];
-        tasks.iter()
-            .for_each(|c| {
-                frqs[*c as usize - 'A' as usize] += 1;
-            });
-
-        frqs.sort_by(|x1, x2| x1.cmp(x2));
-
-        let f_max = frqs[25];
-        let mut idle_time = (f_max - 1) * n;
-
-        for i in (0..=24).rev() {
-            idle_time -= i32::min(f_max - 1, frqs[i as usize]);
-            if idle_time <= 0 {
-                break;
-            }
-        }
-        idle_time = i32::max(0, idle_time);
-
-        idle_time + tasks.len() as i32
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        
     }
 }
 
