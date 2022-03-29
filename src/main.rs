43e22cd31f873c95dabfb1a827184de2b7564d85
f30010776 <futianchi@huawei.com>
2022-03-29 17:52:20 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,10 @@ struct Solution {}
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::u32;
 
-// 34. Find First and Last Position of Element in Sorted Array
+// 670. Maximum Swap
 impl Solution {
-    pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        vec![nums.iter().position(|num| num == &target),
-             nums.iter().rposition(|num| num == &target)]
-            .into_iter()
-            .map(|res| {
-                if let Some(pos) = res {
-                    pos as i32
-                } else {
-                    -1
-                }
-            })
-            .collect::<Vec<_>>()
+    pub fn maximum_swap(num: i32) -> i32 {
+
     }
 }
 
